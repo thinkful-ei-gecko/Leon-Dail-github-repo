58f5d55d0da7e40a9d21aa9bdf49323975107711
Leon Dail <leondailofficial@gmail.com>
2019-08-22 13:36:05 -0700

    finished github project

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 'use strict';
+
+function resultsCode(name,html_url) {
+  $('.repo-list').append(`
+  <li>
+    <h3>Name: ${name}</h3>
+    <p>URL: ${html_url}</p>
+  </li>
+  `);
+}
+
+function results(responseJson, searchData) {
+  $('.repo-list').empty(); //empty previous results
+  $('#search').val(''); //empty search bar
+  $('.username-repos').text(`${searchData}'s Repos`);
+  for (let j = 0;j<responseJson.length;j++) {
+    let name = responseJson[j]['name'];
+    let url = responseJson[j]['html_url'];
+    resultsCode(name,url);
+  }
+}
+function listener() {
+  $('#github-search').on('submit', e=> {
+    e.preventDefault();
+    let searchData = $('#search').val();
+    let url = `https://api.github.com/users/${searchData}/repos`;
+    console.log(url);
+    fetch(url)
+      .then(response => {
+        if (response.ok) { return response.json(); }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => results(responseJson,searchData))
+      .catch(err => {
+        $('.repo-list').empty();
+        $('.username-repos').text(`${err} sorry`);
+      });
+  });
+}
+
+$(listener);
